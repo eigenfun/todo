17216fb433d836bdae565b2554b1fe1f2e56435e
Nash Kamal <eigenfun@gmail.com>
2019-08-27 22:33:55 -0400

    the main app file and login

---
+++ src/Components/App.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Redirect } from 'react-router-dom';
+import AppContent from './AppContent/AppContent';
+import styles from './App.module.css';
+import Login from './Login/Login';
+import AppMain from './AppMain/AppMain';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      logged_in: false, 
+      token: ''
+    }
+  }
+
+  setToken = (token) => {
+    this.setState({token: token})
+  }
+
+  logout = () => {
+    localStorage.removeItem('token')
+    this.setState({logged_in: false, token: ''})
+  }
+
+  render() {
+  return (
+    <Router token={this.state.token}>
+      <div className={styles.app}>
+        <AppContent>
+          <Route path='/login' component={Login} />
+          <Route path='/tasklist' component={AppMain} />
+          <Redirect to='/login' />
+        </AppContent>
+        <Route path="/logout" render={() => {
+          this.logout()
+          return <Redirect to={{ pathname: "/login" }} />;
+          }} />
+      </div>
+    </Router>
+  );
+  }
+}
+
+export default App
---
+++ src/Components/Login/Login.js
@@ -0,0 +1,63 @@
+import React from 'react';
+import styles from './Login.module.css';
+
+import { Container, Button, Form } from "react-bootstrap"
+
+const api_base = "http://localhost:4000"
+
+
+
+export default class Login extends React.Component {
+  
+  constructor(props) {
+    super(props);
+    this.state = {
+      token: this.context
+    };
+  }
+
+  handleSubmit = async e => {
+    e.preventDefault();
+
+    const { username, password } = e.target;
+
+    // Login request
+    try {
+      const data = await fetch(api_base + '/api/login', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          username: username.value,
+          password: password.value
+        })
+      }).then(res => res.json());
+      localStorage.setItem('token', data.token);
+      this.props.history.push('/tasklist')
+
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+  render() {
+    return (
+      <Container style={{marginLeft: '40%'}}>
+        <h2>TODO Login</h2>
+        <Form onSubmit={this.handleSubmit}>
+          <div className={`${styles.inputDiv} pb-1`}>
+            <Form.Label>Username</Form.Label>
+            <Form.Control type="text" name='username' required />
+          </div>
+          <div className={`${styles.inputDiv} pb-1`}>
+            <Form.Label>Password</Form.Label>
+            <Form.Control type="password" name='password' required />
+          </div>
+          <Button type='submit' onSubmit={this.handleSubmit}>Login</Button>
+        </Form>
+      </Container>
+    );
+  }
+
+}
