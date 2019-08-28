726405fa71901ffa127ac2cc395888e7fb9da3ff
Nash Kamal <eigenfun@gmail.com>
2019-08-27 22:25:19 -0400

    adding the standard static content

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './Components/App';
+import './index.css';
+import './table.css';
+import './assets/css/functional.css';
+
+
+ReactDOM.render(<App />, document.getElementById('root'));
+
