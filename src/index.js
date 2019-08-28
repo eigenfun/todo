2a6c7e1e8d592170d9fc96b2bd42cd0a0a2e9469
Nash Kamal <eigenfun@gmail.com>
2019-08-27 23:20:35 -0400

    ran prettifier

--- src/index.js
+++ src/index.js
@@ -4,6 +4,4 @@ import App from './Components/App';
 import './index.css';
 import './assets/css/functional.css';
 
-
 ReactDOM.render(<App />, document.getElementById('root'));
-
--- src/util.js
+++ src/util.js
@@ -1,12 +1,12 @@
 const getAuthBearer = () => {
-    let token = localStorage.getItem('token')
-    var bearer = `Bearer ${token}`;
-    return bearer
-}
+  let token = localStorage.getItem('token');
+  var bearer = `Bearer ${token}`;
+  return bearer;
+};
 
 const getAuthConfig = () => {
-    let bearer = getAuthBearer()
-    return {headers: {'authorization' : bearer }}
-}
+  let bearer = getAuthBearer();
+  return { headers: { authorization: bearer } };
+};
 
-export {getAuthBearer, getAuthConfig}
+export { getAuthBearer, getAuthConfig };
